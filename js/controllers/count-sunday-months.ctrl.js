368bf14d58b09feb51e8ab78fba1b6986986377d
Ikechi Michael <mykehell123@gmail.com>
2017-04-23 14:30:19 +0100

    started on count sundays

---
+++ js/controllers/count-sunday-months.ctrl.js
@@ -0,0 +1,6 @@
+app.controller("CountSundaysCtrl", function ($scope) {
+    $scope.startDate = null;
+    $scope.endDate = null;
+    $scope.months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    $scope.years = [];
+});
